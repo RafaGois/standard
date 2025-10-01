1ae63c0b34c156a393de1b80dc99025fb386fdfa
mendesmaquinas <rafaelfontanabg@gmail.com>
2025-10-01 14:56:50 -0300

    feat: about us animations

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,8 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const aboutUs = document.querySelector('.about-us');
-    const aboutUsTitle = document.querySelector('.about-us-title');
-    const aboutUsDescription = document.querySelector('.about-us-description');
-    const aboutUsImage = document.querySelector('.about-us-image');
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutUs = document.querySelector("#about-us");
+  const aboutUsTitle = document.querySelector(".about-us-title");
+  const aboutUsDescription = document.querySelector(".about-us-description");
+  const aboutUsCards = document.querySelectorAll(".about-us-card");
 
-    
+  const tl = gsap.timeline({
+    paused: true,
+    scrollTrigger: {
+      trigger: aboutUs,
+      start: "top 70%",
+    },
+  });
+
+  tl.from(aboutUsTitle, {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    ease: "power2",
+  });
+
+  tl.from(
+    aboutUsDescription,
+    {
+      opacity: 0,
+      y: 20,
+      duration: 1,
+      ease: "power2",
+    },
+    "-=0.3"
+  );
+
+ 
+    tl.from(
+      document.querySelectorAll(".about-us-card-icon"),
+      {
+        scale: 0,
+        duration: 1,
+        ease: "elastic",
+      }
+    , "<"
+    );
+
+    tl.from(document.querySelectorAll(".about-us-card-title"), {
+      opacity: 0,
+      y: 20,
+      duration: 1,
+      ease: "power2",
+    }, "<");
+
+    tl.from(document.querySelectorAll(".about-us-card-description"), {
+      opacity: 0,
+      y: 20,
+      duration: 1,
+      ease: "power2",
+    }, "<");
+
 });
--- src/js/hero.js
+++ src/js/hero.js
@@ -5,21 +5,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const heroImage = hero.querySelector('.hero-image');
     const heroBtn = hero.querySelector('.hero-btn');
 
+    const headerTl = gsap.timeline();
     const tl = gsap.timeline();
+
+    headerTl.from(document.querySelector('.header-logo'), {
+        scale: 0,
+        duration: 1,
+        ease: "power2",
+    });
+
+    headerTl.from(document.querySelectorAll('.header-link'), {
+        scale: 0,
+        duration: 1,
+        stagger: 0.2,
+        ease: "power2",
+    });
 
     tl.from(heroTitle, {
         opacity: 0,
         y: 20,
         duration: 1,
         ease: "power2",
-    });
+    }, "<");
     
     tl.from(heroDescription, {
         opacity: 0,
         y: 20,
         duration: 1,
         ease: "power2",
-    });
+    }, "-=0.3");
     
     tl.from(heroImage, {
         opacity: 0,
@@ -33,7 +47,5 @@ document.addEventListener('DOMContentLoaded', function() {
         y: 20,
         duration: 1,
         ease: "power2",
-    });
-    
-    
+    }, "<");
 });
